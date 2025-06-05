dfa666fae072d48526335a047b515ebf242156cf
Asya <web.zaslavskaya@gmail.com>
2025-06-05 15:10:02 +0300

    sliderfix

--- wp-content/themes/svadba/src/front/js/owl.js
+++ wp-content/themes/svadba/src/front/js/owl.js
@@ -19,42 +19,58 @@ $('.owl-carousel').owlCarousel({
         }
     }
   })
+
   
-
-let SliderName =  document.querySelector(".owl-item.active  .slideName");
-let placeForName = document.querySelector(".small-gold");
-
-placeForName.innerHTML = SliderName.textContent;
-
-  let owl = $(".owl-carousel");
-      owl.owlCarousel();
-      $(".next-btn").click(function () {
-          owl.trigger("next.owl.carousel");
-          placeForName.innerHTML = SliderName.textContent;
+let UpdateSliderName = function(){ 
+        let SliderName =  document.querySelector(".owl-item.active  .slideName");
+        let placeForName = document.querySelector(".small-gold");
+        placeForName.innerHTML = SliderName.textContent;
 
           if (SliderName.getAttribute('data-link')) {
               placeForName.setAttribute('href', SliderName.getAttribute('data-link'));
-          }
+            }
+
+};
+
+UpdateSliderName();
+
+      let owl = $(".owl-carousel");
+      owl.owlCarousel();
+      $(".next-btn").click(function () {
+          owl.trigger("next.owl.carousel");
+          UpdateSliderName();        
+
 
       });
       $(".prev-btn").click(function () {
           owl.trigger("prev.owl.carousel");
-          placeForName.innerHTML = SliderName.textContent;
 
-          if (SliderName.getAttribute('data-link')) {
-              placeForName.setAttribute('href', SliderName.getAttribute('data-link'));
-          }
+          UpdateSliderName();
+
       });
       $(".prev-btn").addClass("disabled");
       $(owl).on("translated.owl.carousel", function (event) {
           if ($(".owl-prev").hasClass("disabled")) {
               $(".prev-btn").addClass("disabled");
+          
           } else {
               $(".prev-btn").removeClass("disabled");
+
           }
           if ($(".owl-next").hasClass("disabled")) {
               $(".next-btn").addClass("disabled");
+
           } else {
               $(".next-btn").removeClass("disabled");
           }
       });
+      $(owl).on('translated.owl.carousel',  function(){ 
+                    let SliderName =  document.querySelector(".owl-item.active  .slideName");
+                    let placeForName = document.querySelector(".small-gold");
+                    placeForName.innerHTML = SliderName.textContent;
+
+                    if (SliderName.getAttribute('data-link')) {
+                        placeForName.setAttribute('href', SliderName.getAttribute('data-link'));
+                        }
+
+            });
