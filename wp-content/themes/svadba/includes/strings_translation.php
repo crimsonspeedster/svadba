ab0ee076d60e3d650bfe69b032672b8e31d2ab2b
crimson_speedster <shvets.o@mavericks.agency>
2025-06-05 15:07:20 +0200

    [*] fix for string translate

--- wp-content/themes/svadba/includes/strings_translation.php
+++ wp-content/themes/svadba/includes/strings_translation.php
@@ -3,7 +3,7 @@
 if (function_exists('pll_register_string')) {
     pll_register_string('svadba', 'Попередня');
     pll_register_string('svadba', 'Наступна');
-    pll_register_string('svadba', '<a href="%s">Хочу</a> подробиць');
+    pll_register_string('svadba', '<a class="scroll-to-form" href="%s">Хочу</a> подробиць');
     pll_register_string('svadba', 'Швидше, до вашої <br> ідеальної <br> весіллі!');
     pll_register_string('svadba', 'Немає зображення');
     pll_register_string('svadba', '<a href="%s" target="_blank" rel="nofollow noindex noopener">Створення сайту</a> — студія Март');
